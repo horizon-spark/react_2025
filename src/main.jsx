dcaf07ccc6ab4096ae87d07e07a65609ed1b21e4
horizon-spark <166980733+horizon-spark@users.noreply.github.com>
2025-07-06 11:46:35 +0300

    Rendered restaurant data from mock.js (hw-2)

---
+++ src/main.jsx
@@ -0,0 +1,28 @@
+import { createRoot } from "react-dom/client";
+import { restaurants } from "../mock";
+
+const root = document.getElementById("root");
+
+const reactRoot = createRoot(root);
+
+reactRoot.render(
+  <div>
+    {restaurants.map((restaurant) => (
+      <div>
+        <h2>{restaurant.name}</h2>
+        <h3>Меню</h3>
+        <ul>
+          {restaurant.menu.map((dish) => (
+            <li>{dish.name}</li>
+          ))}
+        </ul>
+        <h3>Отзывы</h3>
+        <ul>
+          {restaurant.reviews.map((review) => (
+            <li>{review.text}</li>
+          ))}
+        </ul>
+      </div>
+    ))}
+  </div>
+);
